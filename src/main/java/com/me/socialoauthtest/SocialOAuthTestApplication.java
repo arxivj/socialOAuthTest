cb8e1f91c5349da71563dc964a63e0a67610940d
qsdfdsf <xivpark1110@gmail.com>
2023-05-31 02:15:53 +0900

    :seedling:

---
+++ src/main/java/com/me/socialoauthtest/SocialOAuthTestApplication.java
@@ -0,0 +1,13 @@
+package com.me.socialoauthtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialOAuthTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialOAuthTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/me/socialoauthtest/SocialOAuthTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.socialoauthtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialOAuthTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
